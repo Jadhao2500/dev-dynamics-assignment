4d9738a0c693900ddb63ab1f8446779defc3cf64
Saurabh Jadhao <saurabhjadhao001@gmail.com>
2024-09-04 00:24:41 +0530

    meta activity sample

--- src/components/ActivityMeta.tsx
+++ src/components/ActivityMeta.tsx
@@ -1,4 +1,6 @@
-import { memo } from "react"
+import { VALID_DATA_TYPES } from "../constant";
+import { util } from "../utils/utils";
+import { memo } from "react";
 
 type props = {
     meta: { label: string, fillColor: string }[]
@@ -6,10 +8,11 @@ type props = {
 const ActivityMeta = (props: props) => {
     const { meta } = props
     console.log({ meta })
-    console.log()
-    return <>
-
-    </>
+    return <div className={`flex-between-center g-10`}>
+        {util.validateDataType(meta, VALID_DATA_TYPES.ARRAY) && meta.map((el) => (
+            <p key={el.label} style={{ color: `${el.fillColor}`, background: `${el.fillColor}1A`, padding: "0.5rem", minHeight: "15vh", width: "100%", fontWeight: 600, fontSize: "1.25rem" }}>{el.label}</p>
+        ))}
+    </div>
 }
 
 export default memo(ActivityMeta)
