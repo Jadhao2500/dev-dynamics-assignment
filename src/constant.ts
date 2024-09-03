b8addc370a1770764c233e33cf8d7a4903e11bb5
Saurabh Jadhao <saurabhjadhao001@gmail.com>
2024-09-03 16:23:18 +0530

    app constants

---
+++ src/constant.ts
@@ -0,0 +1,10 @@
+export const VALID_DATA_TYPES = {
+  NUMBER: "number",
+  STRING: "string",
+  ARRAY: "array",
+  OBJECT: "object",
+  NULL: "null",
+  UNDEFINED: "undefined",
+  BOOLEAN: "boolean",
+  FUNCTION: "function",
+};
