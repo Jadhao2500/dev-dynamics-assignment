e72d5cabf4d88f4b0d0b3a2cfe10fd49e7af5311
Saurabh Jadhao <saurabhjadhao001@gmail.com>
2024-09-04 00:23:26 +0530

    use dashboard context

--- src/components/TotalActivityChart.tsx
+++ src/components/TotalActivityChart.tsx
@@ -1,21 +1,26 @@
+import { useDashboardContext } from "@/pages/dashboard/dashboardContext";
 import { memo, useMemo } from "react";
 import { Bar } from "react-chartjs-2";
-type props = {
-    activityData: {
-        name: string;
-        totalActivity: {
-            name: string;
-            value: string
-        }[]
-    }[],
-    color: string[],
-}
-type dataSets = {
-    [key: string | number]: (number)[]
-}
+
+// type props = {
+//     activityData: {
+//         name: string;
+//         totalActivity: {
+//             name: string;
+//             value: string
+//         }[]
+//     }[],
+//     color: string[],
+// }
+
 type labels = string[];
 
-type dataSets2 = { label: string, data: number[], backgroundColor: string[], borderColor: string[], borderWidth: number }[]
+type dataSets = { label: string, data: number[], backgroundColor: string[], borderColor: string[], borderWidth: number }[]
+
+type activity = {
+    name: string;
+    value: string
+}
 
 const options = {
     responsive: true,
@@ -28,52 +33,34 @@ const options = {
     }
 }
 
-const TotalActivityChart = (props: props) => {
-    const { activityData, color } = props;
-    const { labels, dataSets, dataSets2, labels2 }: { labels: labels, dataSets: dataSets, dataSets2: dataSets2, labels2: labels } = useMemo(() => {
-        const labels = activityData.map((el) => el.name);
-        const dataSets: dataSets = {};
-        activityData.forEach((el) => {
-            el.totalActivity.forEach((activity) => {
-                if (!dataSets[activity.name]) {
-                    dataSets[activity.name] = [Number(activity.value)]
-                } else {
-                    dataSets[activity.name].push(Number(activity.value))
-                }
-            })
-        })
-        let dataSets2: dataSets2 = [];
+const TotalActivityChart = () => {
+    const { state } = useDashboardContext();
+
+    const { dataSets2, labels2 }: { dataSets2: dataSets, labels2: labels } = useMemo(() => {
+        let dataSets2: dataSets = [];
         let labels2: string[] = []
-        activityData.forEach((el) => {
+        state.totalActivitiesAnalytics.forEach((el: any) => {
             let set = {
                 label: el.name,
-                data: el.totalActivity.map((el) => {
+                data: el.totalActivity.map((el: activity) => {
                     if (!labels2.includes(el.name)) {
                         labels2.push(el.name)
                     }
                     return Number(el.value)
                 }),
-                backgroundColor: color?.map((el) => `${el}1A`) ?? [""] ?? [""],
-                borderColor: color,
+                backgroundColor: state.color?.map((el: string) => `${el}1A`) ?? [""] ?? [""],
+                borderColor: state.color,
                 borderWidth: 1
             }
             dataSets2.push(set)
         })
 
         return {
-            labels, dataSets, dataSets2, labels2
+            dataSets2, labels2
         }
-    }, [activityData])
+    }, [state.totalActivitiesAnalytics])
 
     console.log({
-        labels,
-        dataSets, color, data: Object.entries(dataSets).map(([key, value]) => ({
-            label: key,
-            data: value,
-            backgroundColor: color,
-            borderColor: color?.map((el) => `${el}1A`),
-
-        })),
         dataSets2,
         labels2
     })
