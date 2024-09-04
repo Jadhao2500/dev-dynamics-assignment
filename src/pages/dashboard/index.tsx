5bd473d210d549c34bab21ddc30b292cf0064ad6
Saurabh Jadhao <saurabhjadhao001@gmail.com>
2024-09-04 22:28:52 +0530

    integrate total activities chart

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,36 +1,38 @@
-import ActivityMeta from "@components/ActivityMeta";
-import sampleData from "../../sample-data.json"
+// import ActivityMeta from "@components/ActivityMeta";
+// import sampleData from "../../sample-data.json"
 import Grid from '@mui/material/Grid2';
 import ActiveDaysChart from "@components/ActiveDaysChart";
-import TotalActivityChart from "@/components/TotalActivityChart";
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, ArcElement, Title, LineElement, Tooltip, Legend } from "chart.js";
+import TotalEmployeeWiseActivityChart from "@/components/TotalEmployeeWiseActivityChart";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, ArcElement, LineElement, Tooltip, Legend } from "chart.js";
 import DayWiseActivity from "@/components/DayWiseActivity";
 import { DashboardProvider } from "./dashboardContext";
 import { useEffect, useReducer } from "react";
 import { ACTION_TYPES, dashboardReducer, initialState } from "./dashboardReducer";
+import TotalActivityChart from "@/components/TotalActivityChart";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, ArcElement, Title, LineElement, Tooltip, Legend)
+ChartJS.register(CategoryScale, LinearScale, BarElement, ArcElement, LineElement, Tooltip, Legend)
 
 const AnalyticalDashboard = () => {
-    const [state, dispatch] = useReducer(dashboardReducer, initialState)
-    console.log({ state })
-    useEffect(()=>{
-        dispatch({type:ACTION_TYPES.SELECTED_DATE,value:"2024-05-06"})
-    },[])
+    const [state, dispatch] = useReducer(dashboardReducer, initialState);
+
+    useEffect(() => {
+        dispatch({ type: ACTION_TYPES.SELECTED_DATE, value: "2024-05-06" })
+    }, []);
+
     return (
         <DashboardProvider value={{ state, dispatch }}>
             <Grid container spacing={2}>
-                <Grid size={12}>
+                {/* <Grid size={12}>
                     <ActivityMeta meta={sampleData.data.AuthorWorklog.activityMeta} />
-                </Grid>
-                <Grid size={4}>
+                </Grid> */}
+                <Grid size={6}>
                     <ActiveDaysChart />
                 </Grid>
-                <Grid size={8}>
-                    <ActiveDaysChart />
+                <Grid size={6}>
+                    <TotalActivityChart />
                 </Grid>
                 <Grid size={12}>
-                    <TotalActivityChart />
+                    <TotalEmployeeWiseActivityChart />
                 </Grid>
                 <Grid size={12}>
                     <DayWiseActivity />
